f961d06d9e0e6537a152f3fbd5afd6caed72f962
FernandoDev19 <fernandocanotapias@gmail.com>
2026-06-14 01:20:25 -0500

    Minor changes

--- menu-web/src/components/Header.tsx
+++ menu-web/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <div className="bg-background shadow-md sticky top-0 z-50">
             <div className="py-5 px-5 flex-row justify-between items-center">
-                <img className="w-auto h-14" src="src/assets/logo.png" alt="Logo Sabor Espress" />
+                <img className="w-auto h-14" src="/logo.png" alt="Logo Sabor Espress" />
             </div>
         </div>
     );
